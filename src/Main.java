fcae2bb92347038510f92ff07ff052d253a372c6
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-05-18 02:39:07 +0200

    updated but

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,28 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
+        // Establish a connection to the database
         Connection connection = null;
         try {
-            // Establish a connection to the database
             connection = DriverManager.getConnection("jdbc:sqlite:database.db");
 
-            // Create a UserDAO object
+            // Create an instance of the UserDAO
             UserDAO userDAO = new UserDAO(connection);
 
-            // Create the user table
+            // Create the user table if it doesn't exist
             userDAO.createUserTable();
 
-            // Insert a user
-            userDAO.insertUser("John Doe", "john@example.com");
+            // Insert a user into the database
+            userDAO.insertUser("John Doe", "john.doe@example.com");
 
-            // Perform more database operations as needed
+            // Retrieve users from the database
+            // You can add more code here to perform additional database operations
 
             // Close the connection
-            connection.close();
+            userDAO.closeConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
