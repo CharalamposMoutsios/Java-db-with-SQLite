fb5c611166fbd28abeedbe5d99eb2628c1bdd4b5
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-05-18 02:12:04 +0200

    add UserDao

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,27 @@
 import java.sql.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            // Establish a connection to the database
+            connection = DriverManager.getConnection("jdbc:sqlite:database.db");
 
+            // Create a UserDAO object
+            UserDAO userDAO = new UserDAO(connection);
 
+            // Create the user table
+            userDAO.createUserTable();
+
+            // Insert a user
+            userDAO.insertUser("John Doe", "john@example.com");
+
+            // Perform more database operations as needed
+
+            // Close the connection
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/UserDAO.java
@@ -0,0 +1,43 @@
+import java.sql.*;
+
+public class UserDAO {
+    private Connection connection;
+
+    public UserDAO(String dbPath) {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createUserTable() {
+        String query = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT)";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertUser(String name, String email) {
+        String query = "INSERT INTO users (name, email) VALUES (?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, name);
+            statement.setString(2, email);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Add additional methods for retrieving users, updating users, deleting users, etc.
+
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
